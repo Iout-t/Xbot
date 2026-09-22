65857d3d2205773e654bd891618d6ff3dabce912
Iout-t <ssweatcatlover@gmail.com>
2026-09-22 09:20:13 +0530

    Add build.gradle.kts for project configuration

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+buildscript {
+    dependencies {
+        classpath("com.android.tools.build:gradle:$agpVersion")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:$hiltVersion")
+        classpath("com.google.protobuf:protobuf-gradle-plugin:$protobufVersion")
+    }
+}
+
+plugins {
+    id("com.android.application") version "$agpVersion" apply false
+    id("org.jetbrains.kotlin.android") version "$kotlinVersion" apply false
+    id("com.google.dagger.hilt") version "$hiltVersion" apply false
+    id("com.google.protobuf") version "$protobufVersion" apply false
+}
+
+allprojects {
+    group = "com.example.automation"
+    version = "1.0.0"
+}
